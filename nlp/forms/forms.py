226a0f1ea791f211c948888be2c3248b97deee56
Иван Иванов <mullosharaf@gmail.com>
2024-05-05 18:49:29 +0300

    first commit

---
+++ nlp/forms/forms.py
@@ -0,0 +1,41 @@
+﻿from django import forms
+
+class CalculatorForm(forms.Form):
+    input_text = forms.CharField(label='Исходной текст', widget=forms.Textarea)    
+    operation = forms.ChoiceField(
+        choices=(   
+            # Список операций с ключами на английском и значениями на русском
+            ("Tokenization", "Токенизация"),
+            ("Lemmatization", "Лемматизация"),
+            ("Stemming", "Стемминг"),
+            ("Word2Vec", "Векторное представление слов"),
+            ("NamedEntityRecognition", "Анализ сущностей"),
+            ("SyntaxAnalysis", "Анализ синтаксиса"),
+            ("MorphologicalAnalysis", "Анализ морфологии"),
+            ("SemanticAnalysis", "Анализ семантики"),
+            ("PartOfSpeechTagging", "Определение частей речи"),
+            ("SentimentAnalysis", "Анализ тональности"),
+            ("EmotionAnalysis", "Анализ эмоций"),         
+            ("KeywordExtraction", "Выделение ключевых слов"), 
+            ("NamedEntityRecognition", "Разметка именованных сущностей"),
+            ("CoreferenceResolution", "Разрешение кореференции"),
+            ("MachineTranslation", "Машинный перевод"),
+            ("TextGeneration", "Генерация текста")
+        ),
+        widget=forms.Select(attrs={'onchange': 'this.form.submit();'}),  # Добавляем атрибут onchange
+        label='Выберите операцию'
+    )
+    librarychoice = forms.ChoiceField(
+        choices=(),
+        widget=forms.Select(),  # Используем выпадающий список для выбора библиотеки
+        label='Выберите библиотеку'
+    )
+    output_text = forms.CharField(label='Результат', widget=forms.Textarea, required=False)
+
+    def __init__(self, libraries, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Получаем значение поля operation из данных формы
+        operation = self.data.get("operation")
+        # Если значение не пустое, устанавливаем соответствующие библиотеки для поля librarychoice
+        if operation:
+            self.fields["librarychoice"].choices = [(lib, lib) for lib in libraries[operation]]
---
+++ nlp/nlpservices/middleware.py
@@ -0,0 +1,40 @@
+﻿from nlp.nlpservices import sentimentAnalyzer
+from nlp.nlpservices.lemmatizer import Lemmatizer
+from nlp.nlpservices.stemmer import Stemmer
+from nlp.nlpservices.tokenizer import Tokenizer
+from nlp.nlpservices.wordEmbedding import WordEmbedding
+
+class Middleware:
+    @staticmethod
+    def tokenization(text, library):
+        tokenizer = Tokenizer(library)
+        tokens = tokenizer.tokenize(text)
+        return ', '.join(tokens)
+
+    @staticmethod
+    def lematization(text, library):
+        lemmatizer = Lemmatizer(library)
+        lemmas = lemmatizer.lemmatize(text)
+        return ', '.join(lemmas)
+
+    @staticmethod
+    def stemming(text, library):
+        stemmer = Stemmer(library)
+        stems = stemmer.stem(text)
+        return ', '.join(stems)
+    
+    @staticmethod
+    def word2vec(text, library):
+        vectors = WordEmbedding(library)
+        vects = vectors.vectorize(text)
+        # Преобразование векторов в строки для объединения
+        vects_str = [', '.join(map(str, vector)) for vector in vects]
+        return vects_str
+    
+    @staticmethod
+    def analyze_sentiment(text, library):        
+        sentiment_analyzer = sentimentAnalyzer.SentimentAnalysis(library)
+        result = sentiment_analyzer.analyze_sentiment(text)
+        formatted_result = "\n".join([f"{item['label']}: {item['score']}" for item in result[0]])
+        return formatted_result    
+    
